d0c86a33985ef3c5f4c3696a206c4242efa2ff1d
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2024-07-11 14:48:00 +0500

    Changes not staged for commit: 	modified:   src/home/components/MessageContainer.jsx 	modified:   src/home/components/Profile.jsx 	modified:   src/home/components/Sidebar.jsx 	modified:   src/login/Login.jsx 	modified:   src/register/Register.jsx

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: "https://backend-test-production-4e1e.up.railway.app",
-        secure: false
-      }
-    }
-  }
+  
 })
